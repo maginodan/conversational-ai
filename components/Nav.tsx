69c76f5b2d1994f630a3c550efe6c48f83505bf8
maginodan <137385172+maginodan@users.noreply.github.com>
2025-06-02 14:08:42 +0300

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -32,9 +32,10 @@ export const Nav = () => {
         "px-4 py-2 flex items-center h-14 z-50 bg-card border-b border-border"
       }
     >
-      <div>
-        <HumeLogo className={"h-5 w-auto"} />
-      </div>
+    <div className="max-w-[80px] max-h-[30px]">
+      <HumeLogo className="w-full h-auto" />
+    </div>
+
       <div className={"ml-auto flex items-center gap-1"}>
         <Button
           onClick={() => {
